6e036b31e760e797a1a1f9b57b3911eee043f61a
KHALID DJAZI <52473469+djazi@users.noreply.github.com>
2024-02-06 14:38:29 +0100

    Update settings.py

--- Stellantis_Djz/Stellantis_Djz/settings.py
+++ Stellantis_Djz/Stellantis_Djz/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'Stellantis DJZ',
         'USER': 'postgres',
-        'PASSWORD': 'Fsociety25800',
+        'PASSWORD': 'adminpostgres',
         'HOST': 'localhost',
         'PORT':'5433',
 
